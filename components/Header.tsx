944e5944ade01acaf95131d1b518e22402c6f1ba
benrhee <thebenrhee@gmail.com>
2023-03-03 10:37:47 -0600

    links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,19 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 export default function Header() {
     return(
         <>
         <h1> Ben Rhee </h1>
+        <div>
+            <button ref="https://www.linkedin.com/in/thebenrhee/">
+
+            <LinkedInIcon />
+            </button>
+            <GitHubIcon/>
+            <LightModeIcon/>
+        </div>
         </>
     )
 }
